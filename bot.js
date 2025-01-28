615147120e38710c36da05b6b1e4dd4f66e9dd4c
Yushxx <109989261+Yushxx@users.noreply.github.com>
2025-01-28 21:54:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -247,3 +247,15 @@ bot.command('deleteprofile', async (ctx) => {
 
 bot.launch();
 console.log('Bot is running!');
+
+// Créez un serveur HTTP simple qui renvoie "I'm alive" lorsque vous accédez à son URL
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("I'm alive");
+    res.end();
+});
+
+// Écoutez le port 8080
+server.listen(8080, () => {
+    console.log("Keep alive server is running on port 8080");
+});
